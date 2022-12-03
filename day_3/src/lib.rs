662f2b4a203aa6ec2884e8fbff3b027f06400b8a
JockeW <wilhelmsson.joakim@gmail.com>
2022-12-03 15:03:11 +0100

    Day 3 completed

--- day_3/src/lib.rs
+++ day_3/src/lib.rs
@@ -1,8 +1,54 @@
-pub fn part_one(input: &str) -> i64 {
+
+const CHARACTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+pub fn part_one(input: &str) -> usize {
+    let mut priorities:Vec<usize> = Vec::new();
+
     for line in input.trim().split('\n') {
-        //
+        let compartments = line.trim().split_at(line.len() / 2);
+        
+        for c in compartments.0.chars() {
+            if compartments.1.contains(c) {
+                let priority = CHARACTERS.chars().position(|ch| ch == c).unwrap() + 1;
+                priorities.push(priority);
+                break;
+            }
+        }
     }
-    0
+    priorities.iter().sum() 
+}
+
+pub fn part_two(input: &str) -> usize {
+    let mut priorities:Vec<usize> = Vec::new();
+    let mut group: Vec<&str> = Vec::new();
+
+    for (i, mut line) in input.trim().split('\n').enumerate() {
+        line = line.trim();
+        if i % 3 != 0 || i == 0 {
+            group.push(line);
+        }
+        else {
+            for c in group[0].chars() {
+                if group[1].contains(c) && group[2].contains(c) {
+                    let priority = CHARACTERS.chars().position(|ch| ch == c).unwrap() + 1;
+                    priorities.push(priority);
+                    break;
+                }
+            }
+
+            group = vec![line];
+        }
+    }
+
+    for c in group[0].chars() {
+        if group[1].contains(c) && group[2].contains(c) {
+            let priority = CHARACTERS.chars().position(|ch| ch == c).unwrap() + 1;
+            priorities.push(priority);
+            break;
+        }
+    }
+
+    priorities.iter().sum() 
 }
 
 #[cfg(test)]
@@ -14,6 +60,11 @@ mod tests {
 
     #[test]
     fn part_one() {
-        assert_eq!(super::part_a(include_str!("input.txt")), 0);
+        assert_eq!(super::part_one(include_str!("input.txt")), 7872);
+    }
+
+    #[test]
+    fn part_two() {
+        assert_eq!(super::part_two(include_str!("input.txt")), 2497);
     }
 }
